114fe35d3a0dfaa01f3e9910ff7859be825805c6
Akai450 <alievakai@icloud.com>
2022-09-15 13:14:20 +0600

    Урок 2 ДЗ

--- Lesson_2_home_work/main.swift
+++ Lesson_2_home_work/main.swift
@@ -1,12 +1,21 @@
+import Foundation
 
 //Урок 2. Домашняя работа
 //№1. Используя функцию составить следующую программу:
 //Нахождение квадрата числа
+//Вариант 1
 func squaredValue (value: Int) {
     print ("The square of number is \(value)")
     print (value * value)
 }
 print (squaredValue(value: 4))
+
+//Вариант 2
+func squareOfValue (value1: Double, value2: Double) {
+    let square = pow(value1, value2)
+    print (square)
+}
+squaredValue(value: 5)
 
 //Нахождение куба числа
 func cubedValue (value: Int) {
@@ -24,17 +33,17 @@ print (perimeterOfCube(sideLength: 5))
 
 //Нахождение площади прямоугольника
 func squareOfRectangle (sideA: Int, sideB: Int) {
-    print ("Value is \(sideA)")
-    print ("Value is \(sideB)")
+    let square = sideA * sideB
+    print ("Площадь прямоугольника равна \(square)")
 }
 print (squareOfRectangle(sideA: 5, sideB: 10))
 
 //Нахождение площади круга
 
-func squareOfCircle (radius: Int, p: Double) {
-    print (Double (radius) * Double (radius) * p)
+func squareOfCircle (radius: Int) {
+    let square = 3.14 * Double((radius * radius))
+    print ("S = p * r * r: \(square)")
 }
-print (squareOfCircle(radius: 5, p: 3.14))
 
 //№2 Создать 4 функции для калькулятора ( + - / *) с двумя операндами и произвести 10 разных вычислений с разными числами
 func addition (_ x: Int, _ y: Int) -> Int {
